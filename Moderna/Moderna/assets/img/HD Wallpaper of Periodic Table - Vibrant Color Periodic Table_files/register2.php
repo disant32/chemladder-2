28bb06b0938a5d1ad7506f84c8e4d6cc7b60d311
disant <disantmistry@gmail.com>
2024-10-23 23:53:26 +0530

    correction2

---
+++ Moderna/Moderna/assets/img/HD Wallpaper of Periodic Table - Vibrant Color Periodic Table_files/register2.php
@@ -0,0 +1,17 @@
+var com_adswizz_register_PROTOCOL_VERSION = '2.2.0';
+
+if (typeof com_adswizz_synchro_listenerid === 'undefined') {
+    var com_adswizz_synchro_listenerid = 'aee0b769d33a6d9e8ffc7be1c4623ed6';
+}
+else {
+    com_adswizz_synchro_listenerid = 'aee0b769d33a6d9e8ffc7be1c4623ed6';
+}
+
+if (typeof com_adswizz_synchro_listnerid === 'undefined') {
+    var com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+else {
+    com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+var aw_0_req_gdpr = false;
+var us_privacy = '';
